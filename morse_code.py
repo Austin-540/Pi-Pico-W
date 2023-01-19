8719f29f60616cc75f621abddc401efa14d7d70c
Austin <105258719+Austin-540@users.noreply.github.com>
2023-01-19 17:08:09 +1300

    Morse code encoding done

--- morse_code.py
+++ morse_code.py
@@ -1,7 +1,5 @@
 import machine
 led = machine.Pin("LED", machine.Pin.OUT)
-
-
 decode = {".-": "A",
           "-...": "B",
           "-.-.": "C",
@@ -42,5 +40,20 @@ decode = {".-": "A",
 
 encode = {v: k for k, v in decode.items()}
 
-print(encode)
-print(encode["S"])
+
+my_word = input("What is your word? >").upper()
+
+my_word_list = []
+
+for i in range(len(my_word)):
+    my_word_list.append(my_word[i])
+
+dots_and_dashes = []
+    
+for i in range(len(my_word_list)):
+    current_letter = my_word_list[i]
+    for key, value in encode.items():
+        if key == current_letter:
+            dots_and_dashes.append(value)
+
+print(dots_and_dashes)
