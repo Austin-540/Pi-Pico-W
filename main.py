1f9e52dde583c71b72f012e5597316836e0b9ae7
Austin <105258719+Austin-540@users.noreply.github.com>
2023-01-19 13:19:33 +1300

    Hello internet from the pico W

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import machine
 import utime
 import network
 from secrets import *
-import ubinascii
+import socket
 
 led = machine.Pin("LED", machine.Pin.OUT)
 
-for _ in range(10):
+for _ in range(5):
     led.toggle()
     utime.sleep(0.1)
     
@@ -19,17 +19,53 @@ wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
 wlan.connect(wifi_name, wifi_password)
 
-while not wlan.isconnected() and wlan.status() >= 0:
-    print("Trying to connect", end="")
-    utime.sleep(0.25)
-    for _ in range(3):
-        print(".", end="")
-        utime.sleep(0.25)
-    print()
-    utime.sleep(0.25)
-        
+max_wait = 10 #seconds
 
-print(wlan.ifconfig())
+led.on()
+while max_wait > 0:
+    if wlan.status() < 0 or wlan.status() >= 3:
+        break
+    max_wait -= 1
+    print("Trying to connect...")
+    utime.sleep(1)
+    
+if wlan.status() !=3:
+    print("Connection Failed!")
+    while True:
+        led.toggle()
+        utime.sleep(0.2)
+else:
+    print("Connected")
+    print("IP = " + wlan.ifconfig()[0])
+    led.off()
 
-mac = ubinascii.hexlify(network.WLAN().config('mac'),':').decode()
-print(mac)
+
+html = """<!DOCTYPE html>
+<html>
+    <head> <title> Pico W </title> </head>
+    <body> <h1> The Pico W </h1>
+    <p> Hello world! </p>
+    </body>
+</html>"""
+
+sta_if = network.WLAN(network.STA_IF)
+addr = socket.getaddrinfo("0.0.0.0", 80)[0][-1]
+s = socket.socket()
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind(addr)
+s.listen(1)
+
+while True:
+    cl, addr = s.accept()
+    cl_file = cl.makefile("rwb", 0)
+    while True:
+        line = cl_file.readline()
+        if not line or line == b'\r\n':
+            break
+    response = html
+    cl.send("HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n")
+    cl.send(response)
+    cl.close()
+
+
+
