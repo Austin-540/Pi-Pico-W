86c190508fa11d14c7c638b158a49a9c259d36b3
Austin <105258719+Austin-540@users.noreply.github.com>
2023-01-21 12:54:54 +1300

    made buttons.py run if the button is held down while plugged in

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ import machine
 from machine import Pin
 import utime
 
+button = Pin(0, Pin.IN, Pin.PULL_UP)   
+
+if button.value() == 0:
+    print("Buttons imported")
+    import buttons
+    
+
 led = machine.Pin("LED", machine.Pin.OUT)
 
 led.on()
